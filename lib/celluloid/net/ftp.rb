6b1b04530ad5e7cab2ab2738c14583ceae9b3ec4
Tiago Cardoso <tiago.cardoso@swisscom.com>
2016-04-12 16:16:47 +0200

    correcting the namespaces

--- lib/celluloid/net/ftp.rb
+++ lib/celluloid/net/ftp.rb
@@ -3,7 +3,7 @@ require 'celluloid/io'
 require 'net/ftp'
 
 module Celluloid::FTPExtensions
-  module BufferedIOExtension
+  module BufferedExtensions
     def initialize(io)
       if Celluloid::IO.evented?
         super(Celluloid::IO::Socket.try_convert(io))
@@ -14,5 +14,5 @@ module Celluloid::FTPExtensions
   end
 end
 
-Net::FTP::BufferedSocket.send :prepend, Celluloid::FTP::BufferedExtensions  
+Net::FTP::BufferedSocket.send :prepend, Celluloid::FTPExtensions::BufferedExtensions  
 
