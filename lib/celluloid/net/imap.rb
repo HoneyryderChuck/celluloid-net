e1763f307bc79c40d5519f01ddaacdc2b647e10c
Tiago Cardoso <tiago.cardoso@swisscom.com>
2016-04-12 16:15:02 +0200

    preparing the imap and pop (these will be hard)

---
+++ lib/celluloid/net/imap.rb
@@ -0,0 +1,10 @@
+require 'timeout/extensions'
+require 'celluloid/io'
+require 'net/imap'
+
+
+module Celluloid
+  module IMAPExtensions
+
+  end
+end
---
+++ lib/celluloid/net/pop.rb
@@ -0,0 +1,12 @@
+require 'timeout/extensions'
+require 'celluloid/io'
+require 'net/pop'
+
+# good idea...
+module Celluloid
+  module Pop3Extensions
+
+    
+
+  end
+end
